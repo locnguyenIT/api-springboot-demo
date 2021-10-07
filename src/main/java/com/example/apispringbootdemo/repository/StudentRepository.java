542ccec4baba4656b838b3f7c417130c1862c0c9
locnguyenIT <86077654+locnguyenIT@users.noreply.github.com>
2021-10-07 18:26:57 +0700

    Update StudentRepository.java

--- src/main/java/com/example/apispringbootdemo/repository/StudentRepository.java
+++ src/main/java/com/example/apispringbootdemo/repository/StudentRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository //Data Access Layer
-public interface StudentRepository extends JpaRepository<Student, Integer> {
+public interface StudentRepository extends JpaRepository<Student, Integer> { //Spring Data JPA
 
     @Query("SELECT s FROM Student s WHERE s.email = ?1")
     Optional<Student> findStudentByEmail(String email);
